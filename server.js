b9700c77e0fded8fdf956b4dbc3c5593e8465da8
starboyds <dhruvaspiring@gmail.com>
2022-07-19 13:14:24 +0530

    added atlas mongodb setup

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const Product = require('./models/product');
 // ************************** Controllers **************************
 
 // ***************** Database Connection Setup ***************************
-mongoose.connect("mongodb://localhost:27017/softwareDb", {useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect("mongodb+srv://starboyds:naruto27@cluster0.m7gdh.mongodb.net/softwareDb?retryWrites=true&w=majority", {useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log("MONGO CONNECTION OPEN!!");
   })
